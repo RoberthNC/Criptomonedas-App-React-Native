ed27e6006f170b71b7016f2690b3480322168697
Roberto Núñez <Roberto>
2023-09-18 11:21:21 -0500

    Primeros pasos en la App de Criptomonedas

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+import Header from './components/Header'
+
+const App = () => {
+  return (
+    <View>
+      <Header />
+    </View>
+  )
+}
+export default App
